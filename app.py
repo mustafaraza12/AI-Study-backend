0304bcf5714120c61a55b466a3c050611b7350a0
mustafaraza12 <mustafaraza0932@gmail.com>
2026-03-21 22:55:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -413,4 +413,6 @@ def flashcard_generator_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
